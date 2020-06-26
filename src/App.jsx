5f456eb2a9f3edb9e14421e697a4197c545f9510
allezz <aerbay@gmail.com>
2020-06-26 14:07:43 +0200

    Set useEffect calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [series, setSeries] = useState();
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        let response = await axios.get(
+          "https://content.viaplay.se/pc-se/serier/samtliga"
+        );
+        setSeries(
+          response.yourDataObject._embedded["viaplay:blocks"][0]._embedded[
+            "viaplay:products"
+          ]
+        );
+      } catch (error) {
+        setErrorMessage(error.response);
+      }
+    })();
+  }, []);
+
   return <div>HALLO</div>;
 };
 
